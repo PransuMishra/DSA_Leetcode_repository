aca919bae5dc5eed5ab299e0d9bae51c45a3f819
Pransu Mishra <pransumishrapm111@gmail.com>
2025-05-02 17:07:56 +0530

    Time: 0 ms (100%), Space: 46 MB (81.79%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.java
+++ 0598-range-addition-ii/0598-range-addition-ii.java
@@ -1,10 +1,23 @@
+// class Solution {
+//     public int maxCount(int m, int n, int[][] ops) {
+//         int [] common = new int [] {m, n};
+//         for(int [] op : ops ){
+//             common[0] = Math.min(common[0], op[0]);
+//             common[1] = Math.min(common[1], op[1]);
+//         }
+//         return common[0] * common[1];
+//     }
+// }
+
 class Solution {
-    public int maxCount(int m, int n, int[][] ops) {
-        int [] common = new int [] {m, n};
-        for(int [] op : ops ){
-            common[0] = Math.min(common[0], op[0]);
-            common[1] = Math.min(common[1], op[1]);
+    public int maxCount(int m, int n, int[] [] ops){
+        int minRows = m;
+        int minCols = n;
+
+        for(int [] op : ops){
+            minRows = Math.min(minRows, op[0]);
+            minCols = Math.min(minCols, op[1]);
         }
-        return common[0] * common[1];
+        return minRows * minCols;
     }
 }
