ef847a6255bbf544e88d9d6d8ce38bf7429e4a6c
Pransu Mishra <pransumishrapm111@gmail.com>
2026-05-02 21:59:42 +0530

    Time: 4 ms (59.28%), Space: 42.3 MB (44.8%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0; 
+        for(int i=0; i<=n; i++){
+            if(isGood(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isGood(int num) {
+        boolean isDifferent = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            // invalid digits
+            if (digit == 3 || digit == 4 || digit == 7) {
+                return false;
+            }
+
+            // digits that change
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                isDifferent = true;
+            }
+
+            num /= 10;
+        }
+
+        return isDifferent;
+    }
+}
