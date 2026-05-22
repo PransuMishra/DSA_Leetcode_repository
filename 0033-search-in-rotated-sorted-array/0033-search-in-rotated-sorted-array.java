52cec896fc4f1153c0199c3625d854e5af397908
Pransu Mishra <pransumishrapm111@gmail.com>
2026-05-22 14:50:10 +0530

    Time: 0 ms (100%), Space: 44.1 MB (10.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public int search(int[] nums, int target) {
         int start = 0, end = nums.length-1;
-        while(start<=end){
+
+        while(start <= end){
             int mid = start + (end - start)/2;
             if(nums[mid] == target){
                 return mid;
             }
+
             if(nums[mid] >= nums[start]){
                 if(nums[start] <= target && target <= nums[mid]){
                     end = mid-1;
@@ -16,7 +18,7 @@ class Solution {
                 if(nums[mid] <= target && target <= nums[end]){
                     start = mid + 1;
                 }else{
-                    end = mid - 1;
+                    end = mid-1;
                 }
             }
         }
